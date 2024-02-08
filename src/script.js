85b73c56f9b52f6dae7d42de2753cfda4468e94c
Jaime-Ginesky <jaimelee79@gmail.com>
2024-02-07 19:19:45 -0800

    added functionality for number generator and guess form in javascript

--- src/script.js
+++ src/script.js
@@ -1,1 +1,36 @@
 'use strict'
+
+
+const replayGame = document.getElementById("replay-game");
+const highScore = document.getElementById("high-score").textContent = 10;
+
+
+const numberRandom = Math.trunc(Math.random() *20) +1; 
+document.getElementById("number-random").textContent = numberRandom;
+
+
+const submitBtn = document.getElementById("submit-btn").addEventListener("click", (e) => {
+    e.preventDefault();
+    const guess = Number(document.querySelector(".input-box").value);
+    console.log(guess, typeof guess);
+
+if (!guess) {
+    document.getElementById('guess-result').textContent = '⛔️ No Number!';
+} else if (guess === numberRandom) {
+      document.getElementById("guess-result").textContent = '🎉 Correct Number!';
+} else if (guess > numberRandom) {
+    document.getElementById('guess-result').textContent = 'Too High!';
+} else if (guess < numberRandom) {
+    document.getElementById('guess-result').textContent = 'Too Low!';
+}
+});
+
+
+replayGame.addEventListener("click", (e) => {
+    e.preventDefault();
+    location.reload();
+    replayGame.innerHtml = "Let's Go";
+});
+
+
+
